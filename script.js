056d6cf020a8c2eeaffcb423116827b18913c024
Argnam <48570721+Argnam@users.noreply.github.com>
2019-07-20 09:12:32 +1000

    kalenar

---
+++ script.js
@@ -0,0 +1,110 @@
+let events = []
+let eventID = 0
+
+let calendar = $('#calendar')
+let monthNames = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+
+let current = new Date()
+let currentYear = current.getFullYear();
+let currentMonth = current.getMonth();
+
+renderCalendar(current.getMonth(), current.getFullYear())
+
+function renderCalendar(month, year) {
+	$('#calendarTable').html('')
+
+	$('#currentDate').html(`${monthNames[month]}/${year}`)
+
+	let daysInMonth = getDaysInMonth(month, year)
+	let firstDay = new Date(year, month).getDay()
+	if (firstDay == 0) firstDay = 7
+
+	// день недели, с которого начинается текущий месяц
+	let currentMonthStartingDay = new Date(currentYear, currentMonth).getDay()
+
+	// количество дней в предыдущем месяце
+	let daysInPreviousMonth = getDaysInMonth(month - 1, year)
+	// высчитываем с какого дня начать отсчёт
+	let previousMonthStartingDay = 0;
+	if (currentMonthStartingDay == 0) {
+		previousMonthStartingDay = daysInPreviousMonth - 5
+	} else {
+		previousMonthStartingDay = daysInPreviousMonth - currentMonthStartingDay + 2
+	}
+
+	let counter = 1;
+	let nextMonth = 1;
+	let date = 1
+	// создание строки
+	for (let row = 0; row < 7; row++) {
+		let i = $(`<div></div>`).appendTo('#calendarTable')
+		// добавление ячейки
+		for (let cell = 0; cell < 7; cell++) {
+			if (row === 0 && cell < firstDay - 1) {
+				i.append(`<span class="gray">${previousMonthStartingDay}</span>`)
+				previousMonthStartingDay++
+
+				// firstDay ->
+			}
+			else if (date > getDaysInMonth(month, year)) {
+				i.append(`<span class="gray">${nextMonth++}</span>`)
+			}
+			else {
+				if (date === current.getDate() && month === current.getMonth() && year === current.getFullYear()) {
+					i.append(`<span class="current" onclick="addEvent(${date}, ${currentMonth}, ${currentYear})">${date}</span>`)
+				} else {
+					i.append(`<span onclick="addEvent(${date}, ${currentMonth}, ${currentYear})">${date}</span>`)
+				}
+				date++
+				counter++;
+			}
+		}
+		// проверка
+		if (counter > getDaysInMonth(month, year))
+			break;
+	}
+}
+
+function next() {
+	currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+	currentMonth = (currentMonth + 1) % 12;
+	renderCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+	currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+	currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+	renderCalendar(currentMonth, currentYear);
+}
+
+function getDaysInMonth(month, year) {
+	return 32 - new Date(year, month, 32).getDate();
+}
+
+function addEvent(day, month, year) {
+	events.push(
+		{
+			id: eventID++,
+			day: day,
+			month: month,
+			year: year,
+			text: ''
+		}
+	)
+	renderEvents()
+}
+
+function renderEvents() {
+	$('#eventsList').html('')
+	events.forEach((event) => {
+		let el = $(`<div class="cell"></div>`).appendTo('#eventsList')
+		el.append(`<p>${event.day}/${(event.month + 1)}/${event.year}</p>`)
+		event.text == '' ? el.append(`<textarea onchange="changeEventText(${event.id}, this.value)" placeholder="Введите заметку"></textarea>`) : el.append(`<textarea onchange="changeEventText(${event.id}, this.value)" placeholder="Введите заметку">${event.text}</textarea>`)
+		$('#eventsList').append('<hr />')	
+	})
+}
+
+function changeEventText(id, value) {
+	events[id].text = value;
+	renderEvents()
+}
